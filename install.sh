0fd59aee5fa2c0f11c3c42dec084a9f255385e08
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-03-07 15:38:33 +0000

    fix sed

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 RESTARTER_DIR=`pwd`
-sed -i "s/REPLACE_ME/${RESTARTER_DIR}/g" elasticsearch-restarter.conf
-sudo cp elasticsearch-restarter.conf /etc/init && sudo ln -s  /etc/init/elasticsearch-restarter.conf /etc/init.d/elasticsearch-restarter
+echo $RESTARTER_DIR
+sed -i "s#REPLACE_ME#$RESTARTER_DIR#g" elasticsearch-restarter.conf
+#sudo cp elasticsearch-restarter.conf /etc/init && sudo ln -s  /etc/init/elasticsearch-restarter.conf /etc/init.d/elasticsearch-restarter

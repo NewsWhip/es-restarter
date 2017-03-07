3f9cf47bf0331377a63af1d1adb12500a0f054b9
Ross Andreucetti <ross.andreucetti@newswhip.com>
2017-03-07 15:40:13 +0000

    remove comment

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 RESTARTER_DIR=`pwd`
 echo $RESTARTER_DIR
 sed -i "s#REPLACE_ME#$RESTARTER_DIR#g" elasticsearch-restarter.conf
-#sudo cp elasticsearch-restarter.conf /etc/init && sudo ln -s  /etc/init/elasticsearch-restarter.conf /etc/init.d/elasticsearch-restarter
+sudo cp elasticsearch-restarter.conf /etc/init && sudo ln -s  /etc/init/elasticsearch-restarter.conf /etc/init.d/elasticsearch-restarter
